9db14f41e2aaea3fd6d96ad37f6d4bdd7f60a0f0
RuchithaManne <68728304+RuchithaManne@users.noreply.github.com>
2020-08-12 01:42:48 +0530

    Create cifar_run3.sh

---
+++ scripts/cifar_run3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+models=(
+    'aug01-advce-tor-WRN_40_10',
+    'aug01-strades6ce-tor-WRN_40_10'
+)
+for i in "${models[@]}"
+do
+    python ./main.py --experiment experiment01 \
+        --no-hooks \
+        --norm inf --eps 0.031 \
+        --dataset cifar10 \
+        --model ${i} \
+        --attack pgd \
+        --random_seed 0
+done
